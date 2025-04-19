638f6b0988ba21fbb55f04d598a9e6bb3e433045
xerxes324 <206946888+xerxes324@users.noreply.github.com>
2025-04-20 01:14:15 +0530

    first_commit

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onload = (initialize)
+
+
+function initialize()
+{
+    console.log("hi")
+    let parent = document.getElementById("maindiv")
+    let addbutton = document.getElementById("add")
+    let subbutton = document.getElementById("sub")
+    let divbutton = document.getElementById("divide")
+    let mulbutton = document.getElementById("multiply")
+    let equalbutton = document.getElementById("equal")
+    let decimalbutton = document.getElementById("decimal")
+    let clearbutton = document.getElementById("clear")
+    let delbutton = document.getElementById("delete")
+
+    let disp = document.getElementById("display")
+    
+}
